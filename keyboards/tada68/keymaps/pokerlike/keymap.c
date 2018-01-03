f8cc76d6ede18d15952f7fda32d6864ec81e08e3
Chris Johnston <githubchrisjohnston@gmail.com>
2018-01-02 21:07:15 -0800

    Added other grv key to pokerlike

--- keyboards/tada68/keymaps/pokerlike/keymap.c
+++ keyboards/tada68/keymaps/pokerlike/keymap.c
@@ -32,19 +32,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
-   * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
+   * |~` | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
    * |----------------------------------------------------------------|
    * |     |   |Up |   |   |   |   |   |ins|   |   |   |   |     |PGUP|
    * |----------------------------------------------------------------|
    * |      |<- |Dn | ->|   |   |   |   |   |   |hme|pgp|        |PGDN|
    * |----------------------------------------------------------------|
-   * |        |   |app |Bl-|BL |BL+|VU-|VU+|MUT|end|pgdn|  McL|MsU|McR |
+   * |        |   |app |Bl-|BL |BL+|VU-|VU+|MUT|end|pgdn|  McL|MsU|McR|
    * |----------------------------------------------------------------|
    * |    |    |    |                       |   |   |    |MsL|MsD|MsR |
    * `----------------------------------------------------------------'
    */
 [_FL] = KEYMAP_ANSI(
-  _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
+  KC_GRV , KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
   _______,_______, KC_UP,_______,_______, _______,_______,_______,KC_INS,_______,_______,_______,_______, _______,KC_PGUP, \
   _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,_______,_______,_______,_______,KC_HOME,KC_PGUP,        _______,KC_PGDN, \
   _______,_______,KC_APP ,BL_DEC, BL_TOGG,BL_INC,KC_VOLD,KC_VOLU,KC_MUTE, KC_END,KC_PGDN,KC_BTN1, KC_MS_U, KC_BTN2, \
